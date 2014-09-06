15d51647071ec0abbb18132d73500dc1f11e53ca
Anselm Strauss <amsibamsi@gmail.com>
2014-09-06 17:02:58 +0200

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject umgebung "0.1.0-SNAPSHOT"
-  :description "Read settings from the environment and JVM system properties"
+  :description "Create a application configuration and read properties from the system and the environment."
   :url "https://github.com/amsibamsi/umgebung"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src/main"]
