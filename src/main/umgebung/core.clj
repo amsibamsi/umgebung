191a855cb58cd8b7113f604670d1e5ea94849405
Anselm Strauss <amsibamsi@gmail.com>
2014-09-06 16:39:49 +0200

    cleaned up core

--- src/main/umgebung/core.clj
+++ src/main/umgebung/core.clj
@@ -4,19 +4,15 @@
 
 (defn prop
   "A property."
-  [key default doc]
-  {:key key
-   :default default
-   :doc doc})
-
-(defn env
-  "Lookup a set of keys in the environment and system properties. Values in the latter overwrite the former. Return a hashmap with keys/values."
-  [keys]
-
-  )
+  ([key default doc]
+   {:key key
+    :default default
+    :doc doc})
+  ([key]
+   {:key key}))
 
 (defn prop-env
-  "Lookup a set of properties in the environment and system properties. Values in the latter overwrite the former. Return a hashmap with keys/properties."
+  "Lookup a set of properties in the environment and system properties. Values in the latter overwrite the former. Return a hashmap with keys/values."
   [props]
   (p/values (s/find-props
               [(s/env)
@@ -24,9 +20,15 @@
               props)))
 
 (defn prop-env-all
-  "Like env, but return the full property instead of just the values."
+  "Like prop-env, but return the full property instead of just the values."
   [props]
   (p/propmap (s/find-props
                [(s/env)
                 (s/sys)]
                props)))
+
+(defn env
+  "Lookup a set of keys in the environment and system properties. Values in the latter overwrite the former. Return a hashmap with keys/values."
+  [keys]
+    (prop-env (map prop
+                   keys)))
--- src/test/umgebung/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns umgebung.core-test
-  (:require [clojure.test :refer :all]
-            [umgebung.core :as c]))
