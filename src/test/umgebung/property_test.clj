e4b315c90faeb73aee5e59c93fe02222411fff89
Anselm Strauss <amsibamsi@gmail.com>
2014-09-06 16:05:48 +0200

    formatting

--- src/test/umgebung/property_test.clj
+++ src/test/umgebung/property_test.clj
@@ -6,8 +6,12 @@
 (deftest propmap
   []
   (testing "Creating property map from a set of properties."
-    (is (= (p/propmap [(c/prop :key1 1 "1")
-                       (c/prop :key2 2 "2")])
+    (is (= (p/propmap [(c/prop :key1
+                               1
+                               "1")
+                       (c/prop :key2
+                               2
+                               "2")])
            {:key1 {:default 1
                    :doc "1"}
             :key2 {:default 2
@@ -16,7 +20,13 @@
 (deftest values
   []
   (testing "Extracting only values of a set of properties."
-    (is (= (p/values [(assoc (c/prop :key1 1 "1") :value 1.0)
-                      (assoc (c/prop :key2 2 "2") :value 2.0)])
+    (is (= (p/values [(assoc (c/prop :key1
+                                     1
+                                     "1")
+                             :value 1.0)
+                      (assoc (c/prop :key2
+                                     2
+                                     "2")
+                             :value 2.0)])
            {:key1 1.0
             :key2 2.0}))))
